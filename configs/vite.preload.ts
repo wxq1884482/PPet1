714624385e56c62f4981603be62e3ce959762c48
AD <95040002+wxq1884482@users.noreply.github.com>
2025-07-07 19:03:27 +0800

    Update vite.preload.ts

--- configs/vite.preload.ts
+++ configs/vite.preload.ts
@@ -2,38 +2,38 @@ import { join } from 'path'
 import { builtinModules } from 'module'
 import { defineConfig } from 'vite'
 
-// export default defineConfig({
-//   mode: process.env.NODE_ENV,
-//   root: join(__dirname, '../src/preload'),
-//   build: {
-//     outDir: '../../dist/preload',
-//     lib: {
-//       entry: 'index.ts',
-//       formats: ['cjs'],
-//     },
-//     minify: process.env.NODE_ENV === 'production',
-//     emptyOutDir: true,
-//     rollupOptions: {
-//       external: [...builtinModules, 'electron'],
-//       output: {
-//         entryFileNames: '[name].cjs',
-//       },
-//     },
-//   },
-//   resolve: {
-//     alias: {
-//       '@src': join(__dirname, '../src'),
-//     },
-//   },
-// })
 export default defineConfig({
+  mode: process.env.NODE_ENV,
+  root: join(__dirname, '../src/preload'),
   build: {
-    target: 'electron-main',  // 明确指定目标为 Electron 主进程
+    outDir: '../../dist/preload',
+    lib: {
+      entry: 'index.ts',
+      formats: ['cjs'],
+    },
+    minify: process.env.NODE_ENV === 'production',
+    emptyOutDir: true,
     rollupOptions: {
-      external: [],           // 取消外部化所有依赖
+      external: [...builtinModules, 'electron'],
+      output: {
+        entryFileNames: '[name].cjs',
+      },
     },
   },
-  define: {
-    __dirname: '__dirname',   // 保留 Node.js 的 __dirname 变量
+  resolve: {
+    alias: {
+      '@src': join(__dirname, '../src'),
+    },
   },
-});
+})
+// export default defineConfig({
+//   build: {
+//     target: 'electron-main',  // 明确指定目标为 Electron 主进程
+//     rollupOptions: {
+//       external: [],           // 取消外部化所有依赖
+//     },
+//   },
+//   define: {
+//     __dirname: '__dirname',   // 保留 Node.js 的 __dirname 变量
+//   },
+// });
