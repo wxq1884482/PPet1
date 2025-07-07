31e79d80680c9dd0d03b67333ae3a31f1dda17b2
AD <95040002+wxq1884482@users.noreply.github.com>
2025-07-07 18:56:37 +0800

    Update vite.main.ts

--- configs/vite.main.ts
+++ configs/vite.main.ts
@@ -2,27 +2,38 @@ import { join } from 'path'
 import { builtinModules } from 'module'
 import { defineConfig } from 'vite'
 
+// export default defineConfig({
+//   mode: process.env.NODE_ENV,
+//   root: join(__dirname, '../src/main'),
+//   build: {
+//     outDir: '../../dist/main',
+//     lib: {
+//       entry: 'index.ts',
+//       formats: ['cjs'],
+//     },
+//     minify: process.env.NODE_ENV === 'production',
+//     emptyOutDir: true,
+//     rollupOptions: {
+//       external: [...builtinModules, 'electron'],
+//       output: {
+//         entryFileNames: '[name].cjs',
+//       },
+//     },
+//   },
+//   resolve: {
+//     alias: {
+//       '@src': join(__dirname, '../src'),
+//     },
+//   },
+// })
 export default defineConfig({
-  mode: process.env.NODE_ENV,
-  root: join(__dirname, '../src/main'),
   build: {
-    outDir: '../../dist/main',
-    lib: {
-      entry: 'index.ts',
-      formats: ['cjs'],
-    },
-    minify: process.env.NODE_ENV === 'production',
-    emptyOutDir: true,
+    target: 'electron-main',  // 明确指定目标为 Electron 主进程
     rollupOptions: {
-      external: [...builtinModules, 'electron'],
-      output: {
-        entryFileNames: '[name].cjs',
-      },
+      external: [],           // 取消外部化所有依赖
     },
   },
-  resolve: {
-    alias: {
-      '@src': join(__dirname, '../src'),
-    },
+  define: {
+    __dirname: '__dirname',   // 保留 Node.js 的 __dirname 变量
   },
-})
+});
