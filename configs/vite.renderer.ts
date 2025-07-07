16e4fdb0aed8b9a6e766ec32c18525334c56768d
AD <95040002+wxq1884482@users.noreply.github.com>
2025-07-07 19:39:24 +0800

    Update vite.renderer.ts

--- configs/vite.renderer.ts
+++ configs/vite.renderer.ts
@@ -65,9 +65,6 @@ export default defineConfig({
   build: {
     emptyOutDir: true,
     outDir: '../../dist/renderer',
-    rollupOptions: {
-      input: join(__dirname, '../src/renderer/index.html'),
-    },
   },
   resolve: {
     alias: {
